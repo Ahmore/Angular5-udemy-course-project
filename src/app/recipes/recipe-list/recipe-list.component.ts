4df7b4274c7c96d45ca739ec461b25661f002fad
Dariusz Szymczyk <dsz@corcode.com>
2018-01-14 18:24:20 +0100

    Recipe list added.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe(
+            "New recipe",
+            "There is the best recipe",
+            "https://paprikaapp.com/media/web/images/help/windows/recipe.png"
+        ),
 
-  constructor() { }
+        new Recipe(
+            "New recipe",
+            "There is the best recipe",
+            "https://paprikaapp.com/media/web/images/help/windows/recipe.png"
+        )
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
