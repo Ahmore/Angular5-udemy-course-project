ed72bae183171a4b4a022276046b9a2f4e7dd481
Dariusz Szymczyk <dsz@corcode.com>
2018-01-20 19:49:13 +0100

    Adding new ingredients added.

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list-edit',
-  templateUrl: './shopping-list-edit.component.html',
-  styleUrls: ['./shopping-list-edit.component.css']
+    selector: 'app-shopping-list-edit',
+    templateUrl: './shopping-list-edit.component.html',
+    styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+    @Output()
+    onIngredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onAdd(name, quantity) {
+        this.onIngredientAdded.emit(new Ingredient(
+            name,
+            quantity
+        ));
+    }
 }
