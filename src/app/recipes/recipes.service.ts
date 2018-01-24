cec3896e3616372dc75d97cb09e7d2c46f168f91
Dariusz Szymczyk <dsz@corcode.com>
2018-01-25 00:35:26 +0100

    Ingredients added to recipes.

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipesService {
     public onRecipeSelect = new EventEmitter<Recipe>();
@@ -8,13 +9,22 @@ export class RecipesService {
         new Recipe(
             "New recipe",
             "There is the best recipe",
-            "https://paprikaapp.com/media/web/images/help/windows/recipe.png"
+            "https://paprikaapp.com/media/web/images/help/windows/recipe.png",
+            [
+                new Ingredient("Meat", 1),
+                new Ingredient("Break", 2)
+            ]
         ),
 
         new Recipe(
             "Another recipe",
             "There is the best recipe",
-            "https://paprikaapp.com/media/web/images/help/windows/recipe.png"
+            "https://paprikaapp.com/media/web/images/help/windows/recipe.png",
+            [
+                new Ingredient("Tomato", 1),
+                new Ingredient("Break", 2),
+                new Ingredient("Cheese", 2)
+            ]
         )
     ];
 
